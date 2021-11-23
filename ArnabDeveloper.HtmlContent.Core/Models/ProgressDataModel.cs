d0d8d020c16cd7e066d1a754cf2eca02d09ccfb8
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-11-23 10:26:30 +0530

    file scoped namespace

--- ArnabDeveloper.HtmlContent.Core/Models/ProgressDataModel.cs
+++ ArnabDeveloper.HtmlContent.Core/Models/ProgressDataModel.cs
@@ -1,7 +1,6 @@
-﻿namespace ArnabDeveloper.HtmlContent.Core.Models
-{
-    /// <summary>
-    /// Progress data model
-    /// </summary>
-    public record ProgressDataModel(int ProgressValue, WebSiteDataModel? Data);
-}
+﻿namespace ArnabDeveloper.HtmlContent.Core.Models;
+
+/// <summary>
+/// Progress data model
+/// </summary>
+public record ProgressDataModel(int ProgressValue, WebSiteDataModel? Data);
--- ArnabDeveloper.HtmlContent.Core/Services/HtmlContentService.cs
+++ ArnabDeveloper.HtmlContent.Core/Services/HtmlContentService.cs
@@ -1,163 +1,162 @@
 ﻿using ArnabDeveloper.HtmlContent.Core.Models;
 using System.Net;
 
-namespace ArnabDeveloper.HtmlContent.Core.Services
+namespace ArnabDeveloper.HtmlContent.Core.Services;
+
+/// <inheritdoc cref="IHtmlContentService"/>
+public class HtmlContentService : IHtmlContentService
 {
-    /// <inheritdoc cref="IHtmlContentService"/>
-    public class HtmlContentService : IHtmlContentService
+    private IList<string> _urls;
+    IList<string> IHtmlContentService.Urls
     {
-        private IList<string> _urls;
-        IList<string> IHtmlContentService.Urls
+        get => _urls;
+        set => _urls = value;
+    }
+
+    /// <summary>
+    /// Creates a new object of HtmlContentService.
+    /// </summary>
+    public HtmlContentService()
+    {
+        _urls = new List<string>();
+    }
+
+    IEnumerable<WebSiteDataModel> IHtmlContentService.GetContent()
+    {
+        if (!((IHtmlContentService)this).Urls.Any())
         {
-            get => _urls;
-            set => _urls = value;
+            throw new ArgumentException("Url collection is empty");
         }
 
-        /// <summary>
-        /// Creates a new object of HtmlContentService.
-        /// </summary>
-        public HtmlContentService()
+        List<WebSiteDataModel> webSiteDataModels = new();
+        foreach (string url in ((IHtmlContentService)this).Urls)
         {
-            _urls = new List<string>();
+            WebSiteDataModel webSiteDataModel = DownloadString(url);
+            webSiteDataModels.Add(webSiteDataModel);
+        }
+        return webSiteDataModels;
+    }
+
+    async Task<IEnumerable<WebSiteDataModel>> IHtmlContentService.GetContentAsync()
+    {
+        if (!((IHtmlContentService)this).Urls.Any())
+        {
+            throw new ArgumentException("Url collection is empty");
         }
 
-        IEnumerable<WebSiteDataModel> IHtmlContentService.GetContent()
+        List<WebSiteDataModel> webSiteDataModels = new();
+        foreach (string url in ((IHtmlContentService)this).Urls)
         {
-            if (!((IHtmlContentService)this).Urls.Any())
-            {
-                throw new ArgumentException("Url collection is empty");
-            }
+            WebSiteDataModel webSiteDataModel = await DownloadStringTaskAsync(url);
+            webSiteDataModels.Add(webSiteDataModel);
+        }
+        return webSiteDataModels;
+    }
 
-            List<WebSiteDataModel> webSiteDataModels = new();
-            foreach (string url in ((IHtmlContentService)this).Urls)
-            {
-                WebSiteDataModel webSiteDataModel = DownloadString(url);
-                webSiteDataModels.Add(webSiteDataModel);
-            }
-            return webSiteDataModels;
+    async IAsyncEnumerable<ProgressDataModel> IHtmlContentService.GetContentAsyncStream()
+    {
+        if (!((IHtmlContentService)this).Urls.Any())
+        {
+            throw new ArgumentException("Url collection is empty");
         }
 
-        async Task<IEnumerable<WebSiteDataModel>> IHtmlContentService.GetContentAsync()
+        List<WebSiteDataModel> webSiteDataModels = new();
+        foreach (string url in ((IHtmlContentService)this).Urls)
         {
-            if (!((IHtmlContentService)this).Urls.Any())
-            {
-                throw new ArgumentException("Url collection is empty");
-            }
+            WebSiteDataModel webSiteDataModel = await DownloadStringTaskAsync(url);
+            webSiteDataModels.Add(webSiteDataModel);
 
-            List<WebSiteDataModel> webSiteDataModels = new();
-            foreach (string url in ((IHtmlContentService)this).Urls)
-            {
-                WebSiteDataModel webSiteDataModel = await DownloadStringTaskAsync(url);
-                webSiteDataModels.Add(webSiteDataModel);
-            }
-            return webSiteDataModels;
+            int progressPercentage = webSiteDataModels.Count * 100 / ((IHtmlContentService)this).Urls.Count;
+            ProgressDataModel progressDataModel = new(progressPercentage, webSiteDataModel);
+
+            yield return progressDataModel;
+        }
+    }
+
+    async Task<IEnumerable<WebSiteDataModel>> IHtmlContentService.GetContentParallelAsync()
+    {
+        if (!((IHtmlContentService)this).Urls.Any())
+        {
+            throw new ArgumentException("Url collection is empty");
         }
 
-        async IAsyncEnumerable<ProgressDataModel> IHtmlContentService.GetContentAsyncStream()
+        List<Task<WebSiteDataModel>> tasks = new();
+        foreach (string url in ((IHtmlContentService)this).Urls)
         {
-            if (!((IHtmlContentService)this).Urls.Any())
-            {
-                throw new ArgumentException("Url collection is empty");
-            }
+            Task<WebSiteDataModel> t = DownloadStringTaskAsync(url);
+            tasks.Add(t);
+        }
+        WebSiteDataModel[] webSiteDataModels = await Task.WhenAll(tasks);
+        return webSiteDataModels;
+    }
 
-            List<WebSiteDataModel> webSiteDataModels = new();
-            foreach (string url in ((IHtmlContentService)this).Urls)
-            {
-                WebSiteDataModel webSiteDataModel = await DownloadStringTaskAsync(url);
-                webSiteDataModels.Add(webSiteDataModel);
-
-                int progressPercentage = webSiteDataModels.Count * 100 / ((IHtmlContentService)this).Urls.Count;
-                ProgressDataModel progressDataModel = new(progressPercentage, webSiteDataModel);
-
-                yield return progressDataModel;
-            }
+    /// <summary>
+    /// Do not block the UI thread because it is inside Task.Run() otherwise
+    /// it block UI thread. Same fast as GetContentParallelAsync() because it
+    /// works in parallel. Async operation inside Parallel.ForEach() is not 
+    /// wait the UI thread. So normal method needs to be used inside 
+    /// Parallel.ForEach().
+    /// </summary>
+    async Task<IEnumerable<WebSiteDataModel>> IHtmlContentService.GetContentParallelForEachAsync()
+    {
+        if (!((IHtmlContentService)this).Urls.Any())
+        {
+            throw new ArgumentException("Url collection is empty");
         }
 
-        async Task<IEnumerable<WebSiteDataModel>> IHtmlContentService.GetContentParallelAsync()
-        {
-            if (!((IHtmlContentService)this).Urls.Any())
-            {
-                throw new ArgumentException("Url collection is empty");
-            }
+        List<WebSiteDataModel> webSiteDataModels = new();
 
-            List<Task<WebSiteDataModel>> tasks = new();
-            foreach (string url in ((IHtmlContentService)this).Urls)
-            {
-                Task<WebSiteDataModel> t = DownloadStringTaskAsync(url);
-                tasks.Add(t);
-            }
-            WebSiteDataModel[] webSiteDataModels = await Task.WhenAll(tasks);
-            return webSiteDataModels;
+        // Wait UI thread
+        await Task.Run(() => Parallel.ForEach(((IHtmlContentService)this).Urls, url =>
+        {
+            WebSiteDataModel webSiteDataModel = DownloadString(url);
+            webSiteDataModels.Add(webSiteDataModel);
+        }));
+
+        // Do not wait UI thread            
+        //await Task.Run(() => Parallel.ForEach(((IDemoMethods)this).Urls, async url =>
+        //{
+        //    WebSiteDataModel webSiteDataModel = await DownloadAsync(url);
+        //    webSiteDataModels.Add(webSiteDataModel);
+        //}));
+
+        return webSiteDataModels;
+    }
+
+    async Task<IEnumerable<WebSiteDataModel>> IHtmlContentService.GetContentParallelForEachProgressAsync(
+        IProgress<ProgressDataModel> progress)
+    {
+        if (!((IHtmlContentService)this).Urls.Any())
+        {
+            throw new ArgumentException("Url collection is empty");
         }
 
-        /// <summary>
-        /// Do not block the UI thread because it is inside Task.Run() otherwise
-        /// it block UI thread. Same fast as GetContentParallelAsync() because it
-        /// works in parallel. Async operation inside Parallel.ForEach() is not 
-        /// wait the UI thread. So normal method needs to be used inside 
-        /// Parallel.ForEach().
-        /// </summary>
-        async Task<IEnumerable<WebSiteDataModel>> IHtmlContentService.GetContentParallelForEachAsync()
+        List<WebSiteDataModel> webSiteDataModels = new();
+
+        await Task.Run(() => Parallel.ForEach(((IHtmlContentService)this).Urls, url =>
         {
-            if (!((IHtmlContentService)this).Urls.Any())
-            {
-                throw new ArgumentException("Url collection is empty");
-            }
+            WebSiteDataModel webSiteDataModel = DownloadString(url);
+            webSiteDataModels.Add(webSiteDataModel);
 
-            List<WebSiteDataModel> webSiteDataModels = new();
+            int progressPercentage = webSiteDataModels.Count * 100 / ((IHtmlContentService)this).Urls.Count;
+            ProgressDataModel progressDataModel = new(progressPercentage, webSiteDataModel);
+            progress.Report(progressDataModel);
+        }));
 
-            // Wait UI thread
-            await Task.Run(() => Parallel.ForEach(((IHtmlContentService)this).Urls, url =>
-            {
-                WebSiteDataModel webSiteDataModel = DownloadString(url);
-                webSiteDataModels.Add(webSiteDataModel);
-            }));
+        return webSiteDataModels;
+    }
 
-            // Do not wait UI thread            
-            //await Task.Run(() => Parallel.ForEach(((IDemoMethods)this).Urls, async url =>
-            //{
-            //    WebSiteDataModel webSiteDataModel = await DownloadAsync(url);
-            //    webSiteDataModels.Add(webSiteDataModel);
-            //}));
+    private WebSiteDataModel DownloadString(string url)
+    {
+        using WebClient webClient = new();
+        WebSiteDataModel webSiteDataModel = new(url, webClient.DownloadString(url));
+        return webSiteDataModel;
+    }
 
-            return webSiteDataModels;
-        }
-
-        async Task<IEnumerable<WebSiteDataModel>> IHtmlContentService.GetContentParallelForEachProgressAsync(
-            IProgress<ProgressDataModel> progress)
-        {
-            if (!((IHtmlContentService)this).Urls.Any())
-            {
-                throw new ArgumentException("Url collection is empty");
-            }
-
-            List<WebSiteDataModel> webSiteDataModels = new();
-
-            await Task.Run(() => Parallel.ForEach(((IHtmlContentService)this).Urls, url =>
-            {
-                WebSiteDataModel webSiteDataModel = DownloadString(url);
-                webSiteDataModels.Add(webSiteDataModel);
-
-                int progressPercentage = webSiteDataModels.Count * 100 / ((IHtmlContentService)this).Urls.Count;
-                ProgressDataModel progressDataModel = new(progressPercentage, webSiteDataModel);
-                progress.Report(progressDataModel);
-            }));
-
-            return webSiteDataModels;
-        }
-
-        private WebSiteDataModel DownloadString(string url)
-        {
-            using WebClient webClient = new();
-            WebSiteDataModel webSiteDataModel = new(url, webClient.DownloadString(url));
-            return webSiteDataModel;
-        }
-
-        private async Task<WebSiteDataModel> DownloadStringTaskAsync(string url)
-        {
-            using WebClient webClient = new();
-            WebSiteDataModel webSiteDataModel = new(url, await webClient.DownloadStringTaskAsync(url));
-            return webSiteDataModel;
-        }
+    private async Task<WebSiteDataModel> DownloadStringTaskAsync(string url)
+    {
+        using WebClient webClient = new();
+        WebSiteDataModel webSiteDataModel = new(url, await webClient.DownloadStringTaskAsync(url));
+        return webSiteDataModel;
     }
 }
